2429161b64fedac8b230a9c24b1c6cf0fe66d742
Gabriel Canto <gabrielcanto2016@gmail.com>
2025-07-16 20:23:40 -0300

    Adiconando o dotenv

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -12,9 +12,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# carrega o env.dev se existir
+dotenv_path = BASE_DIR / 'env.dev'
+if dotenv_path.exists():
+    load_dotenv(dotenv_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
